7633132a0ad80f7e44249a1e7ccb33f88029d72c
Praveen Kumar <praveendasari0065@gmail.com>
2023-06-22 18:17:58 +0530

    first commit

---
+++ src/main/java/com/springbootcrud/repository/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package com.springbootcrud.repository;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springbootcrud.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	List<Employee> findAll();
+
+
+
+
+}
